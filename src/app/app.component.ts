9bcd95e5aa1dc4e5da5a375dbb804d121c3052d5
Raul Bulux <marachbu@gmail.com>
2020-03-18 12:06:42 -0500

    Generacion de crud inicial con boostrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './clases/employee'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularBoostrap';
+  employeeArray:Employee[]=[
+    {id:1,nombre:'Raul',salario:2330,edad:25},
+    {id:2,nombre:'Byron',salario:5000,edad:18},
+    {id:3,nombre:'Brandon',salario:8500,edad:21}
+  ];
+
+  selectedEmployee:Employee= new Employee();
+
+  Envio(){
+
+    if(this.selectedEmployee.id==0){
+      this.selectedEmployee.id=this.employeeArray.length +1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+  
+    this.selectedEmployee =new Employee();
+  }
+  Editar(employeeEdit:Employee){
+    this.selectedEmployee=employeeEdit;
+    // this.selectedEmployee.nombre=employeeEdit.nombre;
+    // this.selectedEmployee.salario=employeeEdit.salario;
+    // this.selectedEmployee.edad=employeeEdit.edad;
+
+  }
 }
